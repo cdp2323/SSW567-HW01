eebc2ff647e61d8854e1df8efa876d688ae59054
cdp2323 <57602737+cdp2323@users.noreply.github.com>
2020-02-26 23:17:10 -0500

    hw05

--- HW01.py
+++ HW01.py
@@ -1,66 +1,61 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed 1/29/2020
-
 @author: danping cai
 """
 
-import unittest     # this makes Python unittest module available
+import unittest  # this makes Python unittest module available
 
-def classifyTriangle(a,b,c):
+
+def classify_triangle(side1, side2, side3):
     """
-    
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the squate of the third side, then return 'Right'
-        
-        
     """
-    if a<=0 or b<=0 or c<=0:
+    if side1 <= 0 or side2 <= 0 or side3 <= 0:
         return "NotATriangle"
     else:
-        if a + b <= c or a + c <= b or b + c <= a:
+        if side1 + side2 <= side3 or side1 + side3 <= side2 or side2 + side3 <= side1:
             return 'NotATriangle'
-        if a == b == c:
+        if side1 == side2 == side3:
             return 'Equilateral'
-        elif a == b or a == c or b == c:
+        elif side1 == side2 or side1 == side3 or side2 == side3:
             return 'Isoceles'
-        elif a*a + b*b == c*c or a*a + c*c == b*b or b*b + c*c == a*a:
-            return 'Right'    
+        elif side1 * side1 + side2 * side2 == side3 * side3 or side1 * side1 + side3 * side3 == \
+                side2 * side2 or side2 * side2 + side3 * side3 == side1 * side1:
+            return 'Right'
         else:
             return "Scalene"
-        
 
 
 class TestTriangles(unittest.TestCase):
-    def test_classifyTriangle(self):
-        a,b,c = (3,4,5)
-        self.assertNotEqual(classifyTriangle(a,b,c),'NotATriangle')
-        a,b,c = (200,999,0)
-        self.assertEqual(classifyTriangle(a,b,c),'NotATriangle')
-        a,b,c = (5,2,7)
-        self.assertEqual(classifyTriangle(a,b,c),'NotATriangle')
-        a,b,c = (2,3,4)
-        self.assertEqual(classifyTriangle(a,b,c),'Scalene')
-        a,b,c = (9,9,9)
-        self.assertEqual(classifyTriangle(a,b,c),'Equilateral')
-        a,b,c = (90,90,90)
-        self.assertNotEqual(classifyTriangle(a,b,c),'Right')
-        a,b,c = (4,4,2)
-        self.assertEqual(classifyTriangle(a,b,c),'Isoceles')
-        
+    """
+    Test class
+    """
+
+    def test_classify_triangle(self):
+        """
+        Test function
+        :return: None
+        """
+        side1, side2, side3 = (3, 4, 5)
+        self.assertNotEqual(classify_triangle(side1, side2, side3), 'NotATriangle')
+        side1, side2, side3 = (200, 999, 0)
+        self.assertEqual(classify_triangle(side1, side2, side3), 'NotATriangle')
+        side1, side2, side3 = (5, 2, 7)
+        self.assertEqual(classify_triangle(side1, side2, side3), 'NotATriangle')
+        side1, side2, side3 = (2, 3, 4)
+        self.assertEqual(classify_triangle(side1, side2, side3), 'Scalene')
+        side1, side2, side3 = (9, 9, 9)
+        self.assertEqual(classify_triangle(side1, side2, side3), 'Equilateral')
+        side1, side2, side3 = (90, 90, 90)
+        self.assertNotEqual(classify_triangle(side1, side2, side3), 'Right')
+        side1, side2, side3 = (4, 4, 2)
+        self.assertEqual(classify_triangle(side1, side2, side3), 'Isoceles')
+
 
 if __name__ == '__main__':
-    try:
-        a = float(input('Please enter the first side length:'))
-        b = float(input('Please enter the second side length:'))
-        c = float(input('Please enter the third side length:'))
-    except ValueError:
-        print("please enter arabic numbers only")
-    else:
-        print(classifyTriangle(a,b,c))
-    
-    unittest.main() 
+    unittest.main()
